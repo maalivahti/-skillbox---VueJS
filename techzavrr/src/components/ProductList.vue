e770c491f676abf118c5f91350c22cd846ac4984
Valerande <maalivahti@yandex.ru>
2022-12-27 16:53:14 +0700

    complete lesson 3.4: products list is divided into components

---
+++ techzavrr/src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem :product="product" v-for="(product, index) in products" :key="index"></ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  props: ['products'],
+  components: { ProductItem },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ techzavrr/src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Ноутбук ASUS Vivobook 15 V5200',
+    price: '35 990',
+    image: '/img/asus-vivobook-v5200.jpg',
+  },
+  {
+    title: 'Ноутбук Huawei MateBook D 15',
+    price: '56 990',
+    image: '/img/huawei-matebook-d.jpg',
+  },
+  {
+    title: 'Ноутбук Lenovo V15 G2 ITL',
+    price: '44 990',
+    image: '/img/lenovo-v15-itl.jpg',
+  },
+  {
+    title: 'Смартфон Huawei Nova 9 SE 8',
+    price: '20 990',
+    image: '/img/huawei-nove-9.jpg',
+  },
+  {
+    title: 'Смартфон Apple iPhone 14 128Gb',
+    price: '77 090',
+    image: '/img/iphone-14.jpg',
+  },
+  {
+    title: 'Смартфон Honor X8 6/128Gb',
+    price: '15 990',
+    image: '/img/honor-x8.jpg',
+  },
+  {
+    title: '55" Телевизор LG OLED55B1RLA, OLED, 4K Ultra HD',
+    price: '134 990',
+    image: '/img/lg-oled-55.jpg',
+  },
+  {
+    title: '50" Телевизор Xiaomi MI TV P1, 4K Ultra HD',
+    price: '34 990',
+    image: '/img/xiaomi-mi-tv.jpg',
+  },
+  {
+    title: 'Процессор Intel Core i5 11400F',
+    price: '13 290',
+    image: '/img/intel-i5-11400.jpg',
+  },
+  {
+    title: 'Процессор AMD Ryzen 5 5600X',
+    price: '15 990',
+    image: '/img/amd-ryzen-5.jpg',
+  },
+  {
+    title: 'Материнская плата MSI MAG B560M MORTAR WIFI',
+    price: '12 790',
+    image: '/img/msi-mag-motar.jpg',
+  },
+  {
+    title: 'Видеокарта GIGABYTE NVIDIA GeForce RTX 3070TI',
+    price: '57 990',
+    image: '/img/gigabyte-3070ti.jpg',
+  },
+];
